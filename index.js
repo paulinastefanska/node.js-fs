f105b980d163541c89f373b27d0fbd3a7e0b0249
Paulina Stefanska <web.paulinastefanska@gmail.com>
2019-06-27 14:30:43 +0200

    Add new function fs.readdir and fs.writeFile

--- index.js
+++ index.js
@@ -28,3 +28,30 @@ fs.readFile('./text.txt', 'utf-8', function(err, data) {
     });
 });
 */
+
+
+//fs.readdir and  fs.writeFile exercise
+var fs = require('fs');
+var folder = "../node.js-fs/";
+
+fs.readdir(folder, function(err, files) { //read catalog and save content as array in files
+  if (err) { //if error write in console
+    console.log(err);
+  }; 
+  files.forEach(function(name) { //go thru all array contents and show it in console
+    console.log(name);
+  });
+  var files = files;
+  var convertedFiles = files.toString(); //array convert use .toString
+  var newLine = convertedFiles.replace(/,/g, ", \r\n"); //replace ',' to global ','
+  fs.writeFile("dirContent.txt", newLine, save); //save download data to file
+});
+
+function save(err) { //function showing message after successfull save
+  if (err) throw err;
+  console.log("Successfully saved !"); //message
+}
+
+function newLine(err, name) {
+  console.log(name);
+}
