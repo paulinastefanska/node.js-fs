2a71d1505f1bdf197392304c7f19dafab0f32ec7
Paulina Stefanska <web.paulinastefanska@gmail.com>
2019-06-27 13:40:03 +0200

    Add text file and fs read/write function

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/* simple exercise with fs
 var fs = require('fs');
 var StatMode = require('stat-mode');
 
@@ -6,4 +7,21 @@ fs.stat('./dog.jpeg', function(err, stats) {
 	var statMode = new StatMode(stats);
     console.log(statMode.toString());
 });
+*/
 
+// read/write file
+var fs = require('fs');
+var colors = require('colors');
+
+fs.readFile('./text.txt', 'utf-8', function(err, data) {
+    console.log('Data before save!'.blue);
+    console.log(data);
+    fs.appendFile('./text.txt', '\n That`s how data looks after save!', function(err) {
+        if (err) throw err;
+        console.log('Saved!'.blue);
+        fs.readFile('./text.txt', 'utf-8', function(err, data) {
+            console.log('Data after save!'.blue)
+            console.log(data);
+        });
+    });
+});
