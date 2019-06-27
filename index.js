662f81d4cedbd19cd8f6c0d89578c17913f4495b
Paulina Stefanska <web.paulinastefanska@gmail.com>
2019-06-27 12:21:01 +0200

    Add decoing result of fs.stats

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var fs = require('fs');
+var StatMode = require('stat-mode');
 
-//show image info
+//show img info - decoding result of the fs.stats
 fs.stat('./dog.jpeg', function(err, stats) {
-    console.log(stats);
+	var statMode = new StatMode(stats);
+    console.log(statMode.toString());
 });
+
